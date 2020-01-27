9d84115c64b5c08b571635f404deeab5b1e83495
Adriel Schmitz <adrielschmitz10@gmail.com>
2020-01-26 23:23:12 -0300

    Cria frontend do devRaadr

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes')
+const cors = require('cors');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-ajht8.mongodb.net/week10?retryWrites=true&w=majority', {
@@ -8,10 +10,11 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-ajht8.mongodb.net/w
   useNewUrlParser: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333); 
+app.listen(3333);
 // Query params: request.query (Filtros, ordenação, paginação...)
 // Route params: request.params (identificar um recurso na alteração ou remoção)
 // Body: request.body (Dados para criação ou alteração de um registro)
